e5597d621d1af08fb1ca4bc2abc4ee2b30a6d019
Mathéo ELIE-GAUMER <matheo.elie-gaumer@neuroo.ai>
2025-06-04 14:25:21 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js ! With Yannis le bg !</a>
+          Welcome to <a href="https://nextjs.org">Next.js ! With Yannis le bg stagging !</a>
         </h1>
 
         <p className={styles.description}>
